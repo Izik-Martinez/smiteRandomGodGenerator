343bdcab55c0d378376854255c478dba21042710
Izik-Martinez <izikma17@yahoo.com>
2022-04-01 10:13:40 -0400

    character icon images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const godSelectionImageBox = document.querySelector(".godSelection");
 const generateRandomGodButton = document.querySelector("button");
 const godIcon = document.querySelector(".godSelectionIcon")
 
-generateRandomGodButton.addEventListener("click", function(){godIcon.src = "images/achilles.jpg"});
+generateRandomGodButton.addEventListener("click", function(){godIcon.src = "images/aphro.jpg"});
 
 // function randomGod(){
 
